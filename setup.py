00461348e02d3f0c28f255cc1bfd8b29fd00a729
Josef Fruehwald <JoFrhwld@gmail.com>
2015-04-17 16:00:51 +0100

    reshuffle for package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chunkup',
+      version='0.1',
+      description='chunks up audio',
+      url='https://github.com/JoFrhwld/chunkup',
+      author='Josef Fruehwald',
+      author_email='jofrhwld@gmail.com',
+      license='MIT',
+      packages=['chunkup'],
+      zip_safe=False)
